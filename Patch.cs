f770c63ebe9868da5d729c51fe176f086c9012a8
Vyacheslav <slava.shilin07@mail.ru>
2023-08-09 13:53:26 +0300

    Update to current version

--- Patch.cs
+++ Patch.cs
@@ -6,12 +6,12 @@ namespace BossDespawn
     [HarmonyPatch]
     internal class Patch
     {
-        [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.FixedUpdate)), HarmonyPostfix]
+        [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.CustomFixedUpdate)), HarmonyPostfix]
         public static void BaseAICanSenseTarget(Humanoid __instance)
         {
-            if(__instance.IsBoss() && !Player.IsPlayerInRange(__instance.transform.position, radius))
+            if (__instance.IsBoss() && !Player.IsPlayerInRange(__instance.transform.position, radius))
             {
-                Debug($"Destroing {__instance.GetHoverName()}");
+                Debug($"Destroing {__instance.GetHoverName()}...");
                 ZNetScene.instance?.Destroy(__instance.gameObject);
             }
         }
